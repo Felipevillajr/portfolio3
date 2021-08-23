02477b743dd942178084f3b131174fd11bce5269
felipevillajr <felipevilla2010@live.com>
2021-08-23 17:25:31 -0400

    added part of the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Portfolio from "./pages/portfolio/portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ client/src/pages/home/home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./home.scss";
+
+export default function home() {
+  return (
+    <div className="home">
+      <section className="hero-section">
+        <div className="hero-banner">
+          <h1>Design with you in mind</h1>
+          <h3>Online Presence Creation</h3>
+          <p>&</p>
+          <h3>Reliable Solutions</h3>
+        </div>
+        <div className="hero-container"></div>
+        <div className="hero-shape">
+          <h2>Engaging digital innovation to inspire and create</h2>
+          <br />
+          <p>Bring cyberspace to your business</p>
+        </div>
+      </section>
+    </div>
+  );
+}
